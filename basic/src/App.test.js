14b36b1465a1975fbaa67ee9594b233a54b36ba6
정석우 <ingal777@naver.com>
2022-03-27 15:39:56 +0900

    fireEvent , test.only , test.skip , jest matcher study

--- basic/src/App.test.js
+++ basic/src/App.test.js
@@ -4,10 +4,15 @@ import App from './App';
 
 test('the counter starts at 0', () => {
   render(<App />);//render = dom의 컴포넌트를 렌더하는 함수(인자로 react컴포넌트가 들어감)
-  
   const counterElement = screen.getByTestId("counter")//screen object를 이용해서 원하는 element에 접근 (id로 접근)
-  
   expect(counterElement).toHaveTextContent(0)  //id가 counter인 element인 텍스트가 0인지 테스트
+});
+
+//플러스 버튼 테스트
+test("plus button has correct text", () => {
+  render(<App />);
+  const plusButtonElement = screen.getByTestId("plus-button");
+  expect(plusButtonElement).toHaveTextContent("+");
 });
 
 //마이너스 버튼 테스트
@@ -16,15 +21,10 @@ test("minus button has correct text", () => {
   const minusButtonElement = screen.getByTestId("minus-button");
   expect(minusButtonElement).toHaveTextContent("-");
 });
-//플러스 버튼 테스트
-test("plus button has correct text", () => {
-  render(<App />);
-  const plusButtonElement = screen.getByTestId("plus-button");
-  expect(plusButtonElement).toHaveTextContent("+");
-});
 
 
-//유저가 발생시키는 액션이벤트에 대한 테스트는 fireEvent으로 진행
+
+//유저가 발생시키는 액션이벤트에 대한 테스트는 fireEvent(react testing library)으로 진행
 //플러스 버튼 클릭스 count증가 했나 테스트
 test("When the + button is pressed, the counter changes to 1", () => {
   render(<App />);
