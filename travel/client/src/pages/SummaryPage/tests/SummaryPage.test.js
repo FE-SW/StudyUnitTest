25d910ce984bc416fe263ce2ff053145502f4485
정석우 <ingal777@naver.com>
2022-03-30 05:27:09 +0900

    mock service worker study

--- travel/client/src/pages/SummaryPage/tests/SummaryPage.test.js
+++ travel/client/src/pages/SummaryPage/tests/SummaryPage.test.js
@@ -8,11 +8,11 @@ test("checkbox and button", () => {
   const checkbox = screen.getByRole("checkbox", {
     name: "주문하려는 것을 확인하셨나요?",
   });
-  expect(checkbox.checked).toEqual(false);
+  expect(checkbox.checked).toEqual(false); //체크박스 초기상태는 false
 
-  //확인버튼 내용,비활성화 확인
-  const confirmButton = screen.getByRole("button", { name: "주문 확인" });
-  expect(confirmButton.disabled).toBeTruthy();
+  //체크박스 비활성화 -> 확인버튼 비활성화
+  const confirmButton = screen.getByRole("button", { name: "주문 확인" }); 
+  expect(confirmButton.disabled).toBeTruthy(); //checkbox 체크안할시 주문확인버튼 비활성화
 });
 
-//basic과 다르게 getByTestId가 아닌 getByRole
+//웬만하면 우선순위 높은getByRole query이용
